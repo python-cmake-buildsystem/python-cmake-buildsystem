1d82f671cb232e2e849c86677ef6caabebdbea7c
David Sansome <me@davidsansome.com>
2011-01-29 13:12:34 +0000

    Show a summary of the extensions that won't be built

--- cmake/Extensions.cmake
+++ cmake/Extensions.cmake
@@ -1,9 +1,27 @@
 function(add_python_extension name)
     parse_arguments(ADD_PYTHON_EXTENSION
-        "SOURCES;LIBRARIES;INCLUDEDIRS"
+        "REQUIRES;SOURCES;LIBRARIES;INCLUDEDIRS"
         ""
         ${ARGN}
     )
+
+    # Check all the things we require are found.
+    set(missing_deps "")
+    foreach(dep ${ADD_PYTHON_EXTENSION_REQUIRES})
+        if(NOT ${dep})
+            set(missing_deps "${missing_deps}${dep} ")
+        endif(NOT ${dep})
+    endforeach(dep)
+
+    # If any dependencies were missing don't include this extension.
+    if(missing_deps)
+        string(STRIP "${missing_deps}" missing_deps)
+        set(extensions_disabled "${extensions_disabled}${name} (not set: ${missing_deps});"
+             CACHE INTERNAL "" FORCE)
+        return()
+    else(missing_deps)
+        set(extensions_enabled "${extensions_enabled}${name};" CACHE INTERNAL "" FORCE)
+    endif(missing_deps)
 
     # Add a prefix to the target name so it doesn't clash with any system
     # libraries that we might want to link against (eg. readline)
@@ -35,3 +53,16 @@ function(add_python_extension name)
 
     install(TARGETS ${target_name} LIBRARY DESTINATION lib/${LIBPYTHON}/lib-dynload)
 endfunction(add_python_extension)
+
+
+function(show_extension_summary)
+    if(extensions_disabled)
+        message(STATUS "")
+        message(STATUS "The following extensions will NOT be built:")
+        message(STATUS "")
+        foreach(line ${extensions_disabled})
+            message(STATUS "    ${line}")
+        endforeach(line)
+        message(STATUS "")
+    endif(extensions_disabled)
+endfunction(show_extension_summary)
