7f9a837002e637b1e2ebccb3b1a8c656e2b7b971
Chuck Atkins <chuck.atkins@kitware.com>
2015-08-12 21:11:50 -0400

    Mark BUILTIN_FOO and ENABLE_FOO as advanced if they use default values.
    Co-authored-by: Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>

--- cmake/Extensions.cmake
+++ cmake/Extensions.cmake
@@ -66,9 +66,15 @@ function(add_python_extension name)
            "Controls whether the \"${name}\" extension will be built"
            ON
     )
+    if(ENABLE_${upper_name})
+        mark_as_advanced(FORCE ENABLE_${upper_name})
+    else()
+        mark_as_advanced(CLEAR ENABLE_${upper_name})
+    endif()
 
     # Add options that the extention is either external to libpython or
-    # builtin.
+    # builtin.  These will be marked as advanced unless different from default
+    # values
     if(NOT ADD_PYTHON_EXTENSION_BUILTIN)
         set(ADD_PYTHON_EXTENSION_BUILTIN ${BUILD_EXTENSIONS_AS_BUILTIN})
     endif()
@@ -76,6 +82,12 @@ function(add_python_extension name)
            "If this is set the \"${name}\" extension will be compiled in to libpython"
            ${ADD_PYTHON_EXTENSION_BUILTIN}
     )
+    if((BUILTIN_${upper_name} AND BUILD_EXTENSIONS_AS_BUILTIN)
+       OR (NOT BUILTIN_${upper_name} AND NOT BUILD_EXTENSIONS_AS_BUILTIN))
+        mark_as_advanced(FORCE BUILTIN_${upper_name})
+    else()
+        mark_as_advanced(CLEAR BUILTIN_${upper_name})
+    endif()
 
     # HACK _ctypes_test should always be shared
     if(${name} STREQUAL "_ctypes_test")
