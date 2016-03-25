python-cmake-buildsystem/python-cmake-buildsystem

commit 161ab72269183c77a02d3668da702af1da69e4b0

Fix `exec_prefix` error occurring when no extension are built shared
This commit adds an empty `PYTHONHOME/lib-dynload` directory to
the build and install tree to respectively fix the following errors:

Build error:

  [...]
  Relocate _sysconfigdata.py and update pybuilddir.txt
  Could not find platform dependent libraries <exec_prefix>
  Consider setting $PYTHONHOME to <prefix>[:<exec_prefix>]

Runtime error:

  cd /home/jchris/python-cmake-buildsystem-install
  $ ./bin/python -c "print('Hello')"
  Could not find platform dependent libraries <exec_prefix>
  Consider setting $PYTHONHOME to <prefix>[:<exec_prefix>]
  Hello

To reproduce the error, the build system was configured using
the following options:

  cmake -DCMAKE_INSTALL_PREFIX=/home/jchris/python-cmake-buildsystem-install \
    -DBUILD_EXTENSIONS_AS_BUILTIN:BOOL=1 \
    -DENABLE_CTYPES_TEST=0 -DENABLE_TESTCAPI=0 \
    ../python-cmake-buildsystem

It turns out that if all modules are `BUILTIN`, there will be no
`lib-dynload` folder. `bin/python` uses this to auto-determine
the 'exec_prefix' and properly generate the `_sysconfigdata.py`
so we create an empty directory just in case to make sure it
will always exist.

Reviewed-by: Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>

CMakeLists.txt
@@ -301,6 +301,12 @@ if(UNIX)
 
 endif(UNIX)
 
+# Create an empty lib-dynload folder, even if we don't have any extensions
+# to go in there.  bin/python uses this to auto-determine the exec_prefix
+# and properly generate the _sysconfigdata.py
+file(MAKE_DIRECTORY "${EXTENSION_BUILD_DIR}")
+install(DIRECTORY ${EXTENSION_BUILD_DIR} DESTINATION ${PYTHONHOME})
+
 if(BUILD_TESTING)
     set(EXTRATESTOPTS -v)
     set(TESTOPTS -l ${EXTRATESTOPTS})

commit d8d7974f062a190cb0dcb67c02e95431d3e479f0

add_python_extension: Add support for NO_INSTALL option.
Also fix a regression introduced in 161ab72 (Fix `exec_prefix` error
occurring when no extension are built shared) that was causing extension
to systematically be installed (independently of the install rule
specified in add_python_extension)

CMakeLists.txt
@@ -440,7 +440,7 @@ endif(UNIX)
 # to go in there.  bin/python uses this to auto-determine the exec_prefix
 # and properly generate the _sysconfigdata.py
 file(MAKE_DIRECTORY "${EXTENSION_BUILD_DIR}")
-install(DIRECTORY ${EXTENSION_BUILD_DIR} DESTINATION ${PYTHONHOME})
+install(CODE "file(MAKE_DIRECTORY \"\${CMAKE_INSTALL_PREFIX}/${EXTENSION_INSTALL_DIR}\")")
 
 if(BUILD_TESTING)
     set(TESTOPTS -l)

cmake/Extensions.cmake
@@ -27,6 +27,7 @@
 #              BUILTIN_[extension_name]=OFF.
 # ALWAYS_BUILTIN: if this is set the module will always be compiled statically into
 #                 libpython.
+# NO_INSTALL:   do not install or package the extension.
 #
 # Two user-settable options are created for each extension added:
 # ENABLE_[extension_name]   defaults to ON.  If set to OFF the extension will
@@ -42,7 +43,7 @@
 # options ENABLE_FOO and BUILTIN_FOO.
 
 function(add_python_extension name)
-    set(options BUILTIN ALWAYS_BUILTIN)
+    set(options BUILTIN ALWAYS_BUILTIN NO_INSTALL)
     set(oneValueArgs)
     set(multiValueArgs REQUIRES SOURCES DEFINITIONS LIBRARIES INCLUDEDIRS)
     cmake_parse_arguments(ADD_PYTHON_EXTENSION
@@ -216,10 +217,12 @@ function(add_python_extension name)
         endif(ADD_PYTHON_EXTENSION_DEFINITIONS)
 
         # XXX Uncomment when CMake >= 2.8.8 is required
-        #install(TARGETS ${target_name}
-        #        ARCHIVE DESTINATION ${ARCHIVEDIR}
-        #        LIBRARY DESTINATION ${EXTENSION_INSTALL_DIR}
-        #        RUNTIME DESTINATION ${EXTENSION_INSTALL_DIR})
+        #if(NOT ADD_PYTHON_EXTENSION_NO_INSTALL)
+        #    install(TARGETS ${target_name}
+        #            ARCHIVE DESTINATION ${ARCHIVEDIR}
+        #            LIBRARY DESTINATION ${EXTENSION_INSTALL_DIR}
+        #            RUNTIME DESTINATION ${EXTENSION_INSTALL_DIR})
+        #endif()
     endif(BUILTIN_${upper_name})
 endfunction(add_python_extension)
 

cmake/add_python_extension_CMakeLists.txt.in
@@ -3,7 +3,9 @@ include_directories(${ADD_PYTHON_EXTENSION_INCLUDEDIRS})
 
 target_link_libraries(${target_name} ${ADD_PYTHON_EXTENSION_LIBRARIES})
 
-install(TARGETS ${target_name}
-        ARCHIVE DESTINATION ${ARCHIVEDIR}
-        LIBRARY DESTINATION ${EXTENSION_INSTALL_DIR}
-        RUNTIME DESTINATION ${EXTENSION_INSTALL_DIR})
+if(NOT ADD_PYTHON_EXTENSION_NO_INSTALL)
+  install(TARGETS ${target_name}
+          ARCHIVE DESTINATION ${ARCHIVEDIR}
+          LIBRARY DESTINATION ${EXTENSION_INSTALL_DIR}
+          RUNTIME DESTINATION ${EXTENSION_INSTALL_DIR})
+endif()