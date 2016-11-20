python-cmake-buildsystem/python-cmake-buildsystem

commit 0fb04a35b04f80a3961ac61b4f5ac68bf603b995

Update python version configuration based on #150

appveyor_dashboard.cmake
@@ -12,9 +12,6 @@ string(REGEX MATCH "([0-9])\\.([0-9]+)\\.([0-9]+)" _match ${PY_VERSION})
 if(_match STREQUAL "")
   message(FATAL_ERROR "Environment variable 'PY_VERSION' is improperly set.")
 endif()
-set(PY_VERSION_MAJOR ${CMAKE_MATCH_1})
-set(PY_VERSION_MINOR ${CMAKE_MATCH_2})
-set(PY_VERSION_PATCH ${CMAKE_MATCH_3})
 
 set(what "$ENV{APPVEYOR_PULL_REQUEST_TITLE}_#$ENV{APPVEYOR_PULL_REQUEST_NUMBER}")
 if("$ENV{APPVEYOR_PULL_REQUEST_NUMBER}" STREQUAL "")
@@ -40,9 +37,7 @@ set(dashboard_model Experimental)
 set(dashboard_track AppVeyor-CI)
 
 set(dashboard_cache "BUILD_LIBPYTHON_SHARED:BOOL=ON
-PY_VERSION_MAJOR:STRING=${PY_VERSION_MAJOR}
-PY_VERSION_MINOR:STRING=${PY_VERSION_MINOR}
-PY_VERSION_PATCH:STRING=${PY_VERSION_PATCH}
+PY_VERSION:STRING=${PY_VERSION}
 ")
 
 function(downloadFile url dest)

circle_dashboard.cmake
@@ -12,9 +12,6 @@ string(REGEX MATCH "([0-9])\\.([0-9]+)\\.([0-9]+)" _match ${PY_VERSION})
 if(_match STREQUAL "")
   message(FATAL_ERROR "Environment variable 'PY_VERSION' is improperly set.")
 endif()
-set(PY_VERSION_MAJOR ${CMAKE_MATCH_1})
-set(PY_VERSION_MINOR ${CMAKE_MATCH_2})
-set(PY_VERSION_PATCH ${CMAKE_MATCH_3})
 
 set(what "#$ENV{CIRCLE_PR_NUMBER}")
 if("$ENV{CIRCLE_PR_NUMBER}" STREQUAL "")
@@ -29,9 +26,7 @@ set(CTEST_TEST_ARGS PARALLEL_LEVEL 8)
 set(dashboard_model Experimental)
 set(dashboard_track Circle-CI)
 
-set(dashboard_cache "PY_VERSION_MAJOR:STRING=${PY_VERSION_MAJOR}
-PY_VERSION_MINOR:STRING=${PY_VERSION_MINOR}
-PY_VERSION_PATCH:STRING=${PY_VERSION_PATCH}
+set(dashboard_cache "PY_VERSION:STRING=${PY_VERSION}
 ")
 
 # Toolchain

travis_dashboard.cmake
@@ -14,9 +14,6 @@ string(REGEX MATCH "([0-9])\\.([0-9]+)\\.([0-9]+)" _match ${PY_VERSION})
 if(_match STREQUAL "")
   message(FATAL_ERROR "Environment variable 'PY_VERSION' is improperly set.")
 endif()
-set(PY_VERSION_MAJOR ${CMAKE_MATCH_1})
-set(PY_VERSION_MINOR ${CMAKE_MATCH_2})
-set(PY_VERSION_PATCH ${CMAKE_MATCH_3})
 
 set(what "#$ENV{TRAVIS_PULL_REQUEST}")
 if($ENV{TRAVIS_PULL_REQUEST} STREQUAL "false")
@@ -31,9 +28,7 @@ set(CTEST_TEST_ARGS PARALLEL_LEVEL 8)
 set(dashboard_model Experimental)
 set(dashboard_track Travis-CI)
 
-set(dashboard_cache "PY_VERSION_MAJOR:STRING=${PY_VERSION_MAJOR}
-PY_VERSION_MINOR:STRING=${PY_VERSION_MINOR}
-PY_VERSION_PATCH:STRING=${PY_VERSION_PATCH}
+set(dashboard_cache "PY_VERSION:STRING=${PY_VERSION}
 ")
 if($ENV{TRAVIS_OS_NAME} STREQUAL "osx" AND ${PY_VERSION} VERSION_GREATER "3.5.0")
   set(dashboard_cache "${dashboard_cache}

commit 1b4058e0c7257a104bcdb14e98b662cf2546ea4e

[appveyor|circle|travis]_dashboard: Fix version selection setting PYTHON_VERSION
This commit fixes regression introduced in 0fb04a3 (Update python version
configuration based on #150)

appveyor_dashboard.cmake
@@ -37,7 +37,7 @@ set(dashboard_model Experimental)
 set(dashboard_track AppVeyor-CI)
 
 set(dashboard_cache "BUILD_LIBPYTHON_SHARED:BOOL=ON
-PY_VERSION:STRING=${PY_VERSION}
+PYTHON_VERSION:STRING=${PY_VERSION}
 ")
 
 function(downloadFile url dest)

circle_dashboard.cmake
@@ -31,7 +31,7 @@ set(dashboard_binary_name build)
 set(dashboard_model Experimental)
 set(dashboard_track Circle-CI)
 
-set(dashboard_cache "PY_VERSION:STRING=${PY_VERSION}
+set(dashboard_cache "PYTHON_VERSION:STRING=${PY_VERSION}
 ")
 
 # Toolchain

travis_dashboard.cmake
@@ -28,7 +28,7 @@ set(CTEST_TEST_ARGS PARALLEL_LEVEL 8)
 set(dashboard_model Experimental)
 set(dashboard_track Travis-CI)
 
-set(dashboard_cache "PY_VERSION:STRING=${PY_VERSION}
+set(dashboard_cache "PYTHON_VERSION:STRING=${PY_VERSION}
 ")
 if($ENV{TRAVIS_OS_NAME} STREQUAL "osx" AND ${PY_VERSION} VERSION_GREATER "3.5.0")
   set(dashboard_cache "${dashboard_cache}