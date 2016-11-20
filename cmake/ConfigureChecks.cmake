python-cmake-buildsystem/python-cmake-buildsystem

commit 046ad41ad1986ec31743d6358b67364448d8d9eb

ConfigureChecks: Add support for system not expecting "libutil" (e.g VxWorks)
See #163

cmake/ConfigureChecks.cmake
@@ -351,15 +351,30 @@ endif()
 find_library(HAVE_LIBTERMCAP termcap)
 
 set(LIBUTIL_LIBRARIES )
-check_function_exists("openpty" HAVE_BUILTIN_OPENPTY)
-if(HAVE_BUILTIN_OPENPTY)
-  # Libutil functions are builtin the environment (e.g emscripten)
-  set(LIBUTIL_LIBRARIES )
-  set(HAVE_LIBUTIL 1)
-else()
-  find_library(HAVE_LIBUTIL util)
-  if(HAVE_LIBUTIL)
-    set(LIBUTIL_LIBRARIES ${HAVE_LIBUTIL})
+set(LIBUTIL_EXPECTED 1)
+
+if(CMAKE_SYSTEM MATCHES "VxWorks\\-7$")
+  set(LIBUTIL_EXPECTED 0)
+  set(HAVE_LIBUTIL 0)
+endif()
+
+if(LIBUTIL_EXPECTED)
+  check_function_exists("openpty" HAVE_BUILTIN_OPENPTY)
+  if(HAVE_BUILTIN_OPENPTY)
+    # Libutil functions are builtin the environment (e.g emscripten)
+    set(LIBUTIL_LIBRARIES )
+    set(HAVE_LIBUTIL 1)
+  else()
+    if(NOT DEFINED HAVE_LIBUTIL OR "${HAVE_LIBUTIL}" STREQUAL "")
+      find_library(HAVE_LIBUTIL util)
+      if(HAVE_LIBUTIL)
+        set(LIBUTIL_LIBRARIES ${HAVE_LIBUTIL})
+      endif()
+      message(STATUS "Found libutil: ${HAVE_LIBUTIL}")
+    endif()
+  endif()
+  if(NOT HAVE_LIBUTIL)
+    message(FATAL_ERROR "Could NOT find libutil (missing: HAVE_LIBUTIL)")
   endif()
 endif()
 

commit 9002c51f36f46d910a3e71187a71232ed3656c76

ConfigureChecks: Ensure LIBUTIL_LIBRARIES is set after first configure
This commit fixes regression introduced in 046ad41 (ConfigureChecks: Add
support for system not expecting "libutil" (e.g VxWorks))

cmake/ConfigureChecks.cmake
@@ -367,10 +367,10 @@ if(LIBUTIL_EXPECTED)
   else()
     if(NOT DEFINED HAVE_LIBUTIL OR "${HAVE_LIBUTIL}" STREQUAL "")
       find_library(HAVE_LIBUTIL util)
-      if(HAVE_LIBUTIL)
-        set(LIBUTIL_LIBRARIES ${HAVE_LIBUTIL})
-      endif()
       message(STATUS "Found libutil: ${HAVE_LIBUTIL}")
+    endif()
+    if(HAVE_LIBUTIL)
+      set(LIBUTIL_LIBRARIES ${HAVE_LIBUTIL})
     endif()
   endif()
   if(NOT HAVE_LIBUTIL)