18176d6c5c9d74661d30076ad1bf56bba70b46ae
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2017-08-01 17:36:24 -0400

    Fix python 2.7.15 vs2015 build and introduce patch management system
    Thanks: Adam Rankin <arankin@robarts.ca>
    Thanks: Dzenan Zukic <dzenan.zukic@kitware.com>
    Thanks: Franklin King <franklin.king@queensu.ca>

---
+++ cmake/PythonApplyPatches.cmake
@@ -0,0 +1,67 @@
+
+set(_x86 "(x86)")  # Indirection required to avoid error related to CMP0053
+find_program(PATCH_EXECUTABLE
+  NAME patch
+  PATHS "$ENV{ProgramFiles}/Git/usr/bin"
+        "$ENV{ProgramFiles${_x86}}/Git/usr/bin"
+        "$ENV{ProgramFiles}/GnuWin32/bin"
+        "$ENV{ProgramFiles${_x86}}/GnuWin32/bin"
+        "$ENV{ProgramFiles}/Git/bin"
+        "$ENV{ProgramFiles${_x86}}/Git/bin"
+  )
+if(NOT PATCH_EXECUTABLE)
+  message(FATAL_ERROR "Could NOT find patch (missing: PATCH_EXECUTABLE)")
+endif()
+
+set(patches_dir "${Python_SOURCE_DIR}/patches")
+
+function(_apply_patches _subdir)
+  if(NOT EXISTS ${patches_dir}/${_subdir})
+    message(STATUS "Skipping patches: Directory '${patches_dir}/${_subdir}' does not exist")
+    return()
+  endif()
+  file(GLOB _patches RELATIVE ${patches_dir} "${patches_dir}/${_subdir}/*.patch")
+  if(NOT _patches)
+    return()
+  endif()
+  message(STATUS "")
+  list(SORT _patches)
+  foreach(patch IN LISTS _patches)
+    set(msg "Applying '${patch}'")
+    message(STATUS "${msg}")
+    set(applied ${PROJECT_BINARY_DIR}/CMakeFiles/patches/${patch}.applied)
+    if(EXISTS ${applied})
+      message(STATUS "${msg} - skipping (already applied)")
+      continue()
+    endif()
+    execute_process(
+      COMMAND ${PATCH_EXECUTABLE} --quiet -p1 -i ${patches_dir}/${patch}
+      WORKING_DIRECTORY ${SRC_DIR}
+      RESULT_VARIABLE result
+      ERROR_VARIABLE error
+      ERROR_STRIP_TRAILING_WHITESPACE
+      OUTPUT_VARIABLE output
+      OUTPUT_STRIP_TRAILING_WHITESPACE
+      )
+    if(result EQUAL 0)
+      message(STATUS "${msg} - done")
+      get_filename_component(_dir ${applied} DIRECTORY)
+      execute_process(COMMAND ${CMAKE_COMMAND} -E make_directory ${_dir})
+      execute_process(COMMAND ${CMAKE_COMMAND} -E touch ${applied})
+    else()
+      message(STATUS "${msg} - failed")
+      message(FATAL_ERROR "${output}\n${error}")
+    endif()
+  endforeach()
+  message(STATUS "")
+endfunction()
+
+# Apply patches
+_apply_patches("${PY_VERSION}")
+_apply_patches("${PY_VERSION}/${CMAKE_SYSTEM_NAME}")
+_apply_patches("${PY_VERSION}/${CMAKE_SYSTEM_NAME}-${CMAKE_C_COMPILER_ID}")
+set(_version ${CMAKE_C_COMPILER_VERSION})
+if(MSVC)
+  set(_version ${MSVC_VERSION})
+endif()
+_apply_patches("${PY_VERSION}/${CMAKE_SYSTEM_NAME}-${CMAKE_C_COMPILER_ID}/${_version}")
---
+++ patches/README.rst
@@ -0,0 +1,49 @@
+CPython Patches
+===============
+
+Fixes and improvements to CPython should first be contributed upstream. The
+`Python Developer’s Guide <https://docs.python.org/devguide/>`_ is a great
+resource to guide you through the process.
+
+That said, there are few scenarios where using `python-cmake-buildsystem` to
+patch CPython is relevant. Learn more about these at https://github.com/python-cmake-buildsystem/cpython.
+
+By default, patches are applied at configuration time. Setting the option
+``PYTHON_APPLY_PATCHES`` to ``OFF`` allows to change this. Note that the
+build system keep track which patches have been applied.
+
+Each patch is documented by adding an entry to the `README.rst` file
+found in the same directory (or a parent directory). Whenever possible,
+references to `bugs.python.org <http://bugs.python.org>`_(bpo) issues,
+corresponding `GitHub PRs <https://github.com/python/cpython/pull/>`_ and
+any related discussions on forums or mailing lists are also provided.
+
+Patches are organized per python version. Patches specific to a system,
+a system+compiler, or a system+compiler+compiler_version are organized in
+corresponding sub-directories::
+
+  patches/<PYTHON_VERSION>
+  patches/<PYTHON_VERSION>/<CMAKE_SYSTEM_NAME>
+  patches/<PYTHON_VERSION>/<CMAKE_SYSTEM_NAME>-<CMAKE_C_COMPILER_ID>
+  patches/<PYTHON_VERSION>/<CMAKE_SYSTEM_NAME>-<CMAKE_C_COMPILER_ID>/<compiler_version>
+
+where
+
+* ``<PYTHON_VERSION>`` is of the form ``X.Y.Z``
+
+* ``<CMAKE_SYSTEM_NAME>`` is a value like ``Darwin``, ``Linux`` or ``Windows``. See
+  corresponding CMake documentation for more details.
+
+* ``<CMAKE_C_COMPILER_ID>`` is a valid like ``Clang``, ``GNU`` or ``MSVC``. See
+  corresponding CMake documentation for more details.
+
+* ``<compiler_version>`` is set to the value of ``CMAKE_C_COMPILER_VERSION`` (e.g ``5.2.1``)
+  except when using Microsoft compiler where it is set to the value of ``MSVC_VERSION`` (e.g ``1900``).
+
+Before being applied, patches are sorted alphabetically. This ensures that
+patch starting with `0001-` is applied before the one starting with `0002-`.
+
+
+Note that for historical reasons, there are still patches found in ``cmake/patches``
+and ``cmake/patches-win32`` subdirectories. These will gradually be organized as
+described above.
