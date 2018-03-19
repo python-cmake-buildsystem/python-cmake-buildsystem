python-cmake-buildsystem/python-cmake-buildsystem

commit b774ba734ac0cd858ace3353e0f5894759c102a5

python3: Build "python3dll" and fix msvc "xxlimited" link issue.

cmake/libpython/generate_libpythonstub_def.cmake
@@ -1,0 +1,14 @@
+# Sanity checks
+foreach(varname INPUT_DEF_FILE OUTPUT_DEF_FILE)
+  if(NOT DEFINED ${varname})
+    message(FATAL_ERROR "Variable '${varname}' is not defined.")
+  endif()
+endforeach()
+
+file(STRINGS ${INPUT_DEF_FILE} def_lines REGEX "^  (.+)=.+$")
+set(stub_def_lines "EXPORTS")
+foreach(line IN LISTS def_lines)
+  string(REGEX REPLACE "^  (.+)=.+$" "${CMAKE_MATCH_1}" updated_line ${line})
+  set(stub_def_lines "${stub_def_lines}${updated_line}\n")
+endforeach()
+file(WRITE ${OUTPUT_DEF_FILE} "${stub_def_lines}")

cmake/Extensions.cmake
@@ -160,7 +160,12 @@ function(add_python_extension name)
         #    INCLUDE_DIRECTORIES ${ADD_PYTHON_EXTENSION_INCLUDEDIRS})
 
         if(WIN32)
-            list(APPEND ADD_PYTHON_EXTENSION_LIBRARIES libpython-shared)
+            string(REGEX MATCH "Py_LIMITED_API" require_limited_api "${ADD_PYTHON_EXTENSION_DEFINITIONS}")
+            if(require_limited_api STREQUAL "")
+              list(APPEND ADD_PYTHON_EXTENSION_LIBRARIES libpython-shared)
+            else()
+              list(APPEND ADD_PYTHON_EXTENSION_LIBRARIES libpython3-shared)
+            endif()
         endif()
 
         # XXX When CMake >= 2.8.8 is required, remove the section below

cmake/libpython/CMakeLists.txt
@@ -482,6 +482,46 @@ if(BUILD_SHARED)
             SOURCE ${SRC_DIR}/Modules/posixmodule.c
             APPEND PROPERTY COMPILE_DEFINITIONS WITH_NEXT_FRAMEWORK)
     endif(APPLE)
+
+    if(IS_PY3 AND MSVC)
+        # XXX Add BuildPython3_dDef
+
+        # Generate 'python3stub.def'
+        set(pythonstub_def ${CMAKE_BINARY_DIR}/${LIBPYTHON_ARCHIVEDIR}/${CMAKE_CFG_INTDIR}/python3stub.def)
+        add_custom_command(
+            OUTPUT ${pythonstub_def}
+            COMMAND ${CMAKE_COMMAND}
+                -DINPUT_DEF_FILE:PATH=${SRC_DIR}/PC/python3.def
+                -DOUTPUT_DEF_FILE:PATH=${PROJECT_BINARY_DIR}/CMakeFiles/python3stub.def
+                -P ${CMAKE_CURRENT_SOURCE_DIR}/generate_libpythonstub_def.cmake
+            COMMAND ${CMAKE_COMMAND} -E copy_if_different
+                ${PROJECT_BINARY_DIR}/CMakeFiles/python3stub.def
+                ${pythonstub_def}
+        )
+        add_custom_target(generate_libpythonstub_def DEPENDS ${pythonstub_def})
+
+        # Build 'python3.dll'
+        set(targetname "libpython3-shared")
+        add_library(${targetname} SHARED ${SRC_DIR}/PC/python3dll.c ${SRC_DIR}/PC/python3.def)
+        set_target_properties(${targetname} PROPERTIES
+            OUTPUT_NAME python3
+            LIBRARY_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/${LIBPYTHON_LIBDIR}
+            RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/${LIBPYTHON_LIBDIR}
+            INSTALL_NAME_DIR ${CMAKE_INSTALL_PREFIX}/${LIBPYTHON_LIBDIR}
+        )
+        add_dependencies(${targetname} generate_libpythonstub_def)
+
+        # Build 'python3stub.lib' before linking 'python3.dll'
+        set(python3stub_lib ${CMAKE_BINARY_DIR}/${LIBPYTHON_ARCHIVEDIR}/${CMAKE_CFG_INTDIR}/python3stub.lib)
+        add_custom_command(
+            TARGET ${targetname} PRE_LINK
+            COMMAND lib /nologo /def:${pythonstub_def} /out:${python3stub_lib} /MACHINE:X64
+            COMMENT "Rebuilding python3stub.lib"
+            VERBATIM
+        )
+
+        target_link_libraries(${targetname} ${python3stub_lib})
+    endif()
 endif(BUILD_SHARED)
 
 if(BUILD_STATIC)

commit 56cd21599a0fd849acba7c9eedcf12961069d9e5

python3: Fix link error by improving generation of python3stub.def
This commit fixes regression originally introduced in b774ba7 (python3: Build
"python3dll" and fix msvc "xxlimited" link issue.)

The previous implementation of the "generate_libpythonstub_def" module
was omitting the last line of "python3.def". This was causing the following
link error:

Severity	Code	Description	Project	File	Line	Suppression State
Error	LNK2001	unresolved external symbol _Py_VaBuildValue_SizeT	libpython3-shared	D:\T\python-cmake-buildsystem-build\CMakeBuild\libpython\python3.def	1

cmake/libpython/generate_libpythonstub_def.cmake
@@ -6,9 +6,11 @@ foreach(varname INPUT_DEF_FILE OUTPUT_DEF_FILE)
 endforeach()
 
 file(STRINGS ${INPUT_DEF_FILE} def_lines REGEX "^  (.+)=.+$")
-set(stub_def_lines "EXPORTS")
+set(stub_def_lines "EXPORTS\n")
 foreach(line IN LISTS def_lines)
-  string(REGEX REPLACE "^  (.+)=.+$" "${CMAKE_MATCH_1}" updated_line ${line})
-  set(stub_def_lines "${stub_def_lines}${updated_line}\n")
+  string(REGEX MATCH "^  (.+)=.+$" output ${line})
+  if(NOT "${output}" STREQUAL "")
+    set(stub_def_lines "${stub_def_lines}${CMAKE_MATCH_1}\n")
+  endif()
 endforeach()
 file(WRITE ${OUTPUT_DEF_FILE} "${stub_def_lines}")