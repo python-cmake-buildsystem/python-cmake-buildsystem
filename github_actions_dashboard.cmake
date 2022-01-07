35365473bc65ebbd9369f36e884a303d48e8ce65
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2022-01-07 12:49:18 -0500

    Add "GitHub-Actions" dashboard script to test macOS

---
+++ github_actions_dashboard.cmake
@@ -0,0 +1,50 @@
+# Client maintainer: jchris.fillionr@kitware.com
+
+# Sanity checks
+foreach(name IN ITEMS
+  PY_VERSION
+  RUNNER_OS
+  RUNNER_ARCH
+  CC
+  CXX
+  )
+  if("$ENV{${name}}" STREQUAL "")
+    message(FATAL_ERROR "Environment variable '${name}' is not set")
+  endif()
+endforeach()
+
+# Extract major/minor/patch python versions
+set(PY_VERSION $ENV{PY_VERSION})
+string(REGEX MATCH "([0-9])\\.([0-9]+)\\.([0-9]+)" _match ${PY_VERSION})
+if(_match STREQUAL "")
+  message(FATAL_ERROR "Environment variable 'PY_VERSION' is improperly set.")
+endif()
+
+set(CTEST_SITE "$ENV{RUNNER_OS}-$ENV{RUNNER_ARCH}")
+set(CTEST_DASHBOARD_ROOT $ENV{GITHUB_WORKSPACE})
+set(CTEST_SOURCE_DIRECTORY $ENV{GITHUB_WORKSPACE}/src)
+
+set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
+
+set(CTEST_BUILD_FLAGS "-j4")
+set(CTEST_TEST_ARGS PARALLEL_LEVEL 8)
+
+# Build name
+string(SUBSTRING $ENV{GITHUB_SHA} 0 7 commit)
+set(what "#$ENV{GITHUB_HEAD_REF}")
+if("$ENV{GITHUB_HEAD_REF}" STREQUAL "")
+  set(what "$ENV{GITHUB_REF_NAME}")
+endif()
+set(CTEST_BUILD_NAME "${PY_VERSION}_x64-${what}_${commit}")
+
+set(dashboard_binary_name build)
+set(dashboard_model Experimental)
+set(dashboard_track GitHub-Actions)
+
+set(dashboard_cache "BUILD_LIBPYTHON_SHARED:BOOL=ON
+PYTHON_VERSION:STRING=${PY_VERSION}
+")
+
+# Include driver script
+include(${CTEST_SCRIPT_DIRECTORY}/python_common.cmake)
+
