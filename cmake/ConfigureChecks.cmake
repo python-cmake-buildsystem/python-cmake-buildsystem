python-cmake-buildsystem/python-cmake-buildsystem

commit 5cf5b76791fa5e393923c21a28a0b719cbf48a9e

Add support for building Python 3.8.x
* Add checksums for Python 3.8.0 to 3.8.5 and set
  default python version to 3.8.5

* Add support for building new modules:
  _posixshmem, _testinternalcapi

Options:

* Add option WITH_DECIMAL_CONTEXTVAR
  See python/cpython@4d7012410
      [3.8] bpo-39794: Add --without-decimal-contextvar (GH-18702)

* Add option WITH_TRACE_REFS
  See python/cpython@f4e4703e7
      bpo-36465: Make release and debug ABI compatible (GH-12615)

New modules:

* Add _posixshmem
  See python/cpython@e5ef45b8f
      bpo-35813: Added shared_memory submodule of multiprocessing. (GH-11664)

* Add _testinternalcapi
  See python/cpython@23bace26e
      bpo-36635: Add _testinternalcapi module (GH-12841)

Updated modules:

* Remove zipimport C implementation and update command-line
  option associated with _freeze_importlib executable.
  - Remove hard-coded entries from config_3.c.in. For python <= 3.7,
    the entries are added automatically because the extension is marked
    as ALWAYS_BUILTIN
  See python/cpython@79d1c2e6c
      bpo-25711: Rewrite zipimport in pure Python. (GH-6809)

* On windows, link _socket module against iphlpapi
  See python/cpython@8f96c9f8e
      bpo-37007: Implement socket.if_nametoindex(), if_indextoname() and if_nameindex() on Windows (GH-13522)

* On windows, expect pre-built ffi library
  See python/cpython@32119e10b
      bpo-35947: Update Windows to the current version of libffi (GH-11797)

Python interpreter sources:

* Update PARSER_COMMON_SOURCES list used in libpython, pgen, _freeze_importlib targets:
  - Add Parser/myreadline.c, Parser/parsetok.c, Parser/token.c, Parser/tokenizer.c
  - Remove Parser/bitset.c, Parser/firstsets.c, Parser/grammar.c, Parser/metagrammar.c, Parser/pgen.c
  Exclude pgen target that was depending on these source files:
  - Parser/parsetok_pgen.c, Parser/pgenmain.c,  Parser/printgrammar.c, Parser/tokenizer_pgen.c

cmake/ConfigureChecks.cmake
@@ -185,18 +185,20 @@ message(STATUS "${_msg} [${WITH_HASH_ALGORITHM}]")
 # * The Python implementation (always 'cpython-' for us)
 # * The major and minor version numbers
 # * --with-pydebug (adds a 'd')
-# * --with-pymalloc (adds a 'm')
-# * --with-wide-unicode (adds a 'u')
 #
 # Thus for example, Python 3.2 built with wide unicode, pydebug, and pymalloc,
 # would get a shared library ABI version tag of 'cpython-32dmu' and shared
 # libraries would be named 'foo.cpython-32dmu.so'.
+#
+# In Python 3.2 and older, --with-wide-unicode added a 'u' flag.
+# In Python 3.7 and older, --with-pymalloc added a 'm' flag.
+
 set(_msg "Checking ABIFLAGS")
 set(ABIFLAGS )
 if(Py_DEBUG)
   set(ABIFLAGS "${ABIFLAGS}d")
 endif()
-if(WITH_PYMALLOC)
+if(WITH_PYMALLOC AND PY_VERSION VERSION_LESS "3.8")
   set(ABIFLAGS "${ABIFLAGS}m")
 endif()
 message(STATUS "${_msg} - ${ABIFLAGS}")
@@ -211,6 +213,17 @@ endif()
 set(SOABI "cpython-${PY_VERSION_MAJOR}${PY_VERSION_MINOR}${ABIFLAGS}-${PLATFORM_TRIPLET}")
 
 message(STATUS "${_msg} - ${SOABI}")
+
+endif()
+
+if(PY_VERSION VERSION_GREATER_EQUAL "3.8")
+
+# Alternative SOABI used in debug build to load C extensions built in release mode
+# Release and debug (Py_DEBUG) ABI are compatible, but not Py_TRACE_REFS ABI
+if(Py_DEBUG AND NOT WITH_TRACE_REFS)
+  string(REPLACE "d" "" ALT_ABIFLAGS "${ABIFLAGS}")
+  set(ALT_SOABI "cpython-${PY_VERSION_MAJOR}${PY_VERSION_MINOR}${ALT_ABIFLAGS}-${PLATFORM_TRIPLET}")
+endif()
 
 endif()
 
@@ -272,6 +285,12 @@ set(LINUX_NETLINK_HEADERS ${LINUX_NETLINK_HEADERS} linux/netlink.h)
 check_include_files("${LINUX_NETLINK_HEADERS}" HAVE_LINUX_NETLINK_H)
 
 if(IS_PY3)
+set(LINUX_QRTR_HEADERS)
+add_cond(LINUX_QRTR_HEADERS HAVE_ASM_TYPES_H  asm/types.h)
+add_cond(LINUX_QRTR_HEADERS HAVE_SYS_SOCKET_H sys/socket.h)
+set(LINUX_QRTR_HEADERS ${LINUX_QRTR_HEADERS} linux/qrtr.h)
+check_include_files("${LINUX_QRTR_HEADERS}" HAVE_LINUX_QRTR_H)
+
 # On Linux, can.h and can/raw.h require sys/socket.h
 set(LINUX_CAN_HEADERS)
 add_cond(LINUX_CAN_HEADERS HAVE_SYS_SOCKET_H sys/socket.h)
@@ -284,6 +303,7 @@ add_cond(LINUX_VM_SOCKETS_HEADERS HAVE_SYS_SOCKET_H sys/socket.h)
 check_include_files("${LINUX_VM_SOCKETS_HEADERS};linux/vm_sockets.h" HAVE_LINUX_VM_SOCKETS_H)
 endif()
 
+check_include_files(mach-o/dyld.h HAVE_MACH_O_DYLD_H)
 check_include_files(memory.h HAVE_MEMORY_H) # libffi and cpython
 check_include_files(minix/config.h HAVE_MINIX_CONFIG_H)
 check_include_files(ncurses.h HAVE_NCURSES_H)
@@ -350,10 +370,12 @@ check_include_files(stdarg.h HAVE_STDARG_PROTOTYPES)
 if(IS_PY3)
 check_include_files(endian.h HAVE_ENDIAN_H)
 check_include_files(sched.h HAVE_SCHED_H)
+check_include_files(linux/memfd.h HAVE_LINUX_MEMFD_H)
 check_include_files(linux/random.h HAVE_LINUX_RANDOM_H)
 check_include_files(sys/devpoll.h HAVE_SYS_DEVPOLL_H)
 check_include_files(sys/endian.h HAVE_SYS_ENDIAN_H)
 check_include_files(sys/ioctl.h HAVE_SYS_IOCTL_H)
+check_include_files(sys/memfd.h HAVE_SYS_MEMFD_H)
 check_include_files("sys/types.h;sys/kern_control.h" HAVE_SYS_KERN_CONTROL_H)
 check_include_files(sys/sendfile.h HAVE_SYS_SENDFILE_H)
 check_include_files(sys/syscall.h HAVE_SYS_SYSCALL_H)
@@ -467,6 +489,8 @@ python_platform_test(
 cmake_pop_check_state()
 endif()
 
+set_required_def(_POSIX_THREADS 1)    # Define on Linux as it is required for threading
+set_required_def(_POSIX_SOURCE 1)     # Define on Linux in order for 'stat' and other things to work.
 set_required_def(_GNU_SOURCE 1)       # Define on Linux to activate all library features
 set_required_def(_NETBSD_SOURCE 1)    # Define on NetBSD to activate all library features
 set_required_def(__BSD_VISIBLE 1)     # Define on FreeBSD to activate all library features
@@ -481,7 +505,6 @@ set_required_def(__EXTENSIONS__ 1)    # Defined on Solaris to see additional fun
 
 
 if(HAVE_MINIX_CONFIG_H)
-  set_required_def(_POSIX_SOURCE 1)   # Define to 1 if you need to in order for 'stat' and other things to work.
   set_required_def(_POSIX_1_SOURCE 2) # Define to 2 if the system does not provide POSIX.1 features except with this defined.
   set_required_def(_MINIX 1)          # Define to 1 if on MINIX.
 endif()
@@ -750,9 +773,12 @@ add_cond(CFG_HEADERS HAVE_WCHAR_H wchar.h)
 if(IS_PY3)
 add_cond(CFG_HEADERS HAVE_DIRENT_H dirent.h)
 add_cond(CFG_HEADERS HAVE_ENDIAN_H endian.h)
+add_cond(CFG_HEADERS HAVE_LINUX_MEMFD_H linux/memfd.h)
+add_cond(CFG_HEADERS HAVE_MACH_O_DYLD_H mach-o/dyld.h)
 add_cond(CFG_HEADERS HAVE_NET_IF_H net/if.h)
 add_cond(CFG_HEADERS HAVE_SCHED_H sched.h)
 add_cond(CFG_HEADERS HAVE_SYS_ENDIAN_H sys/endian.h)
+add_cond(CFG_HEADERS HAVE_SYS_MEMFD_H sys/memfd.h)
 add_cond(CFG_HEADERS HAVE_SYS_RESOURCE_H sys/resource.h)
 add_cond(CFG_HEADERS HAVE_SYS_SENDFILE_H sys/sendfile.h)
 add_cond(CFG_HEADERS HAVE_SYS_TIME_H sys/time.h)
@@ -903,10 +929,15 @@ check_symbol_exists(_getpty      "${CFG_HEADERS}" HAVE__GETPTY)
 
 if(IS_PY3)
 check_symbol_exists(accept4      "${CFG_HEADERS}" HAVE_ACCEPT4)
+check_symbol_exists(copy_file_range "${CFG_HEADERS}" HAVE_COPY_FILE_RANGE)
 check_symbol_exists(dup3         "${CFG_HEADERS}" HAVE_DUP3)
+check_symbol_exists(_dyld_shared_cache_contains_path "${CFG_HEADERS}" HAVE_DYLD_SHARED_CACHE_CONTAINS_PATH)
+check_symbol_exists(explicit_bzero  "${CFG_HEADERS}" HAVE_EXPLICIT_BZERO)
+check_symbol_exists(explicit_memset "${CFG_HEADERS}" HAVE_EXPLICIT_MEMSET)
 check_symbol_exists(faccessat    "${CFG_HEADERS}" HAVE_FACCESSAT)
 check_symbol_exists(fchmodat     "${CFG_HEADERS}" HAVE_FCHMODAT)
 check_symbol_exists(fchownat     "${CFG_HEADERS}" HAVE_FCHOWNAT)
+check_symbol_exists(fdwalk       "${CFG_HEADERS}" HAVE_FDWALK)
 check_symbol_exists(fexecve      "${CFG_HEADERS}" HAVE_FEXECVE)
 check_symbol_exists(fdopendir    "${CFG_HEADERS}" HAVE_FDOPENDIR)
 check_symbol_exists(fstatat      "${CFG_HEADERS}" HAVE_FSTATAT)
@@ -915,13 +946,19 @@ check_symbol_exists(futimes      "${CFG_HEADERS}" HAVE_FUTIMES)
 check_symbol_exists(futimesat    "${CFG_HEADERS}" HAVE_FUTIMESAT)
 check_symbol_exists(getentropy   "${CFG_HEADERS}" HAVE_GETENTROPY)
 python_check_function(getpriority HAVE_GETPRIORITY)
+check_symbol_exists(getgrgid_r   "${CFG_HEADERS}" HAVE_GETGRGID_R)
+check_symbol_exists(getgrnam_r   "${CFG_HEADERS}" HAVE_GETGRNAM_R)
 check_symbol_exists(getgrouplist "${CFG_HEADERS}" HAVE_GETGROUPLIST)
+check_symbol_exists(getpwnam_r   "${CFG_HEADERS}" HAVE_GETPWNAM_R)
+check_symbol_exists(getpwuid_r   "${CFG_HEADERS}" HAVE_GETPWUID_R)
 check_symbol_exists(htole64      "${CFG_HEADERS}" HAVE_HTOLE64)
 check_symbol_exists(if_nameindex "${CFG_HEADERS}" HAVE_IF_NAMEINDEX)
 check_symbol_exists(linkat       "${CFG_HEADERS}" HAVE_LINKAT)
 check_symbol_exists(lockf        "${CFG_HEADERS}" HAVE_LOCKF)
 check_symbol_exists(lutimes      "${CFG_HEADERS}" HAVE_LUTIMES)
+check_symbol_exists(madvise      "${CFG_HEADERS}" HAVE_MADVISE)
 check_symbol_exists(mbrtowc      "${CFG_HEADERS}" HAVE_MBRTOWC)
+check_symbol_exists(memfd_create "${CFG_HEADERS}" HAVE_MEMFD_CREATE)
 check_symbol_exists(memrchr      "${CFG_HEADERS}" HAVE_MEMRCHR)
 check_symbol_exists(mkdirat      "${CFG_HEADERS}" HAVE_MKDIRAT)
 check_symbol_exists(mkfifoat     "${CFG_HEADERS}" HAVE_MKFIFOAT)
@@ -931,6 +968,7 @@ check_symbol_exists(pipe2        "${CFG_HEADERS}" HAVE_PIPE2)
 check_symbol_exists(posix_fadvise          "${CFG_HEADERS}" HAVE_POSIX_FADVISE)
 check_symbol_exists(posix_fallocate        "${CFG_HEADERS}" HAVE_POSIX_FALLOCATE)
 check_symbol_exists(posix_spawn            "${CFG_HEADERS}" HAVE_POSIX_SPAWN)
+check_symbol_exists(posix_spawnp           "${CFG_HEADERS}" HAVE_POSIX_SPAWNP)
 check_symbol_exists(pread                  "${CFG_HEADERS}" HAVE_PREAD)
 check_symbol_exists(preadv                 "${CFG_HEADERS}" HAVE_PREADV)
 check_symbol_exists(preadv2                "${CFG_HEADERS}" HAVE_PREADV2)
@@ -947,6 +985,7 @@ check_symbol_exists(pwritev2               "${CFG_HEADERS}" HAVE_PWRITEV2)
 check_symbol_exists(readlinkat             "${CFG_HEADERS}" HAVE_READLINKAT)
 check_symbol_exists(readv                  "${CFG_HEADERS}" HAVE_READV)
 check_symbol_exists(renameat               "${CFG_HEADERS}" HAVE_RENAMEAT)
+check_symbol_exists(rtpSpawn               "${CFG_HEADERS}" HAVE_RTPSPAWN)
 check_symbol_exists(sched_rr_get_interval  "${CFG_HEADERS}" HAVE_SCHED_RR_GET_INTERVAL)
 check_symbol_exists(sched_setaffinity      "${CFG_HEADERS}" HAVE_SCHED_SETAFFINITY)
 check_symbol_exists(sched_setparam         "${CFG_HEADERS}" HAVE_SCHED_SETPARAM)
@@ -956,7 +995,9 @@ check_symbol_exists(sethostname            "${CFG_HEADERS}" HAVE_SETHOSTNAME)
 check_symbol_exists(setpriority            "${CFG_HEADERS}" HAVE_SETPRIORITY)
 check_symbol_exists(sched_get_priority_max "${CFG_HEADERS}" HAVE_SCHED_GET_PRIORITY_MAX)
 check_symbol_exists(sigaltstack            "${CFG_HEADERS}" HAVE_SIGALTSTACK)
+check_symbol_exists(sigfillset             "${CFG_HEADERS}" HAVE_SIGFILLSET)
 check_symbol_exists(sigpending             "${CFG_HEADERS}" HAVE_SIGPENDING)
+check_symbol_exists(strsignal              "${CFG_HEADERS}" HAVE_STRSIGNAL)
 check_symbol_exists(sigtimedwait           "${CFG_HEADERS}" HAVE_SIGTIMEDWAIT)
 check_symbol_exists(sigwait                "${CFG_HEADERS}" HAVE_SIGWAIT)
 check_symbol_exists(sigwaitinfo            "${CFG_HEADERS}" HAVE_SIGWAITINFO)
@@ -987,6 +1028,52 @@ check_struct_has_member("struct passwd" pw_gecos  "${CFG_HEADERS}" HAVE_STRUCT_P
 check_struct_has_member("struct passwd" pw_passwd "${CFG_HEADERS}" HAVE_STRUCT_PASSWD_PW_PASSWD)
 
 check_struct_has_member("struct siginfo_t" si_band "${CFG_HEADERS}" HAVE_SIGINFO_T_SI_BAND)
+endif()
+
+#######################################################################
+#
+# Check for shm
+#
+#######################################################################
+if(PY_VERSION VERSION_GREATER_EQUAL "3.8")
+
+function(_write_shm_test func test_filepath_var)
+  set(check_src ${PROJECT_BINARY_DIR}/CMakeFiles/${func}.c)
+  file(WRITE ${check_src} "
+    #ifdef __cplusplus
+    extern \"C\"
+    #endif
+    #ifdef HAVE_SYS_MMAN_H
+    #  include <sys/mman.h>
+    #endif
+    int main() { return ${func}(0, NULL); }
+  ")
+  set(${test_filepath_var} ${check_src} PARENT_SCOPE)
+endfunction()
+foreach(func IN ITEMS shm_open shm_unlink)
+  _write_shm_test(${func} check_src)
+  string(TOUPPER ${func} _func_upper)
+  python_platform_test(
+    HAVE_${_func_upper}
+    "Checking for ${func}"
+    ${check_src}
+    DIRECT
+    )
+  if(NOT HAVE_${_func_upper})
+    cmake_push_check_state()
+    list(APPEND CMAKE_REQUIRED_LIBRARIES rt)
+    python_platform_test(
+      HAVE_${_func_upper}_IN_RT
+      "Checking for ${func} in -lrt"
+      ${check_src}
+      DIRECT
+      )
+    cmake_pop_check_state()
+    set(SHM_NEEDS_LIBRT ${HAVE_${_func_upper}_IN_RT})
+    set(HAVE_${_func_upper} ${HAVE_${_func_upper}_IN_RT})
+  endif()
+endforeach()
+
 endif()
 
 #######################################################################
@@ -1848,6 +1935,7 @@ else()
 endif()
 check_symbol_exists(pthread_sigmask "${CFG_HEADERS}" HAVE_PTHREAD_SIGMASK)
 check_symbol_exists(pthread_atfork "${CFG_HEADERS}" HAVE_PTHREAD_ATFORK)
+check_symbol_exists(pthread_condattr_setclock "${CFG_HEADERS}" HAVE_PTHREAD_CONDATTR_SETCLOCK)
 check_symbol_exists(pthread_getcpuclockid "${CFG_HEADERS}" HAVE_PTHREAD_GETCPUCLOCKID)
 
 add_cond(CFG_HEADERS  HAVE_SEMAPHORE_H  semaphore.h)
@@ -2698,13 +2786,13 @@ endif()
 
 ##########################################################
 
-if(ZLIB_LIBRARY)
+if(ZLIB_LIBRARIES)
   cmake_push_check_state()
   set(CFG_HEADERS_SAVE ${CFG_HEADERS})
 
   set(CFG_HEADERS ${CFG_HEADERS} zlib.h)
-  add_cond(CMAKE_REQUIRED_INCLUDES ZLIB_INCLUDE_DIR ${ZLIB_INCLUDE_DIR})
-  add_cond(CMAKE_REQUIRED_LIBRARIES ZLIB_LIBRARY ${ZLIB_LIBRARY})
+  add_cond(CMAKE_REQUIRED_INCLUDES ZLIB_INCLUDE_DIRS ${ZLIB_INCLUDE_DIRS})
+  add_cond(CMAKE_REQUIRED_LIBRARIES ZLIB_LIBRARIES ${ZLIB_LIBRARIES})
   check_symbol_exists(inflateCopy      "${CFG_HEADERS}" HAVE_ZLIB_COPY)
 
   set(CFG_HEADERS ${CFG_HEADERS_SAVE})

cmake/extensions/CMakeLists.txt
@@ -106,9 +106,11 @@ add_python_extension(xxlimited REQUIRES IS_PY3 BUILD_TESTING
     NO_INSTALL
 )
 add_python_extension(xxsubtype BUILTIN REQUIRES IS_PY3 SOURCES xxsubtype.c)
+if(PY_VERSION VERSION_LESS "3.8")
 # The zipimport module is always imported at startup. Having it as a
 # builtin module avoids some bootstrapping problems and reduces overhead.
 add_python_extension(zipimport ALWAYS_BUILTIN REQUIRES IS_PY3 SOURCES zipimport.c DEFINITIONS Py_BUILD_CORE)
+endif()
 
 # Python 3.6
 if(PY_VERSION VERSION_GREATER_EQUAL "3.6")
@@ -134,6 +136,11 @@ if(PY_VERSION VERSION_GREATER_EQUAL "3.7")
     add_python_extension(_xxtestfuzz SOURCES _xxtestfuzz/_xxtestfuzz.c
                                              _xxtestfuzz/fuzzer.c
     )
+endif()
+
+# Python 3.8
+if(PY_VERSION VERSION_GREATER_EQUAL "3.8")
+    add_python_extension(_testinternalcapi SOURCES _testinternalcapi.c DEFINITIONS "PY3_DLLNAME=\"python3$<$<CONFIG:Debug>:_d>\"")
 endif()
 
 # UNIX-only extensions
@@ -182,6 +189,11 @@ add_python_extension(_scproxy
 # Linux-only extensions
 add_python_extension(linuxaudiodev REQUIRES LINUX IS_PY2 SOURCES linuxaudiodev.c)
 add_python_extension(ossaudiodev REQUIRES LINUX SOURCES ossaudiodev.c)
+
+# Python 3.8: Linux-only extensions
+if(PY_VERSION VERSION_GREATER_EQUAL "3.8")
+    add_python_extension(_posixshmem REQUIRES LINUX SOURCES _multiprocessing/posixshmem.c LIBRARIES "rt")
+endif()
 
 # Windows-only extensions
 add_python_extension(_msi
@@ -263,7 +275,7 @@ if(WIN32)
         DEFINITIONS EAI_ADDRFAMILY
                     HAVE_SOCKADDR_STORAGE
                     HAVE_ADDRINFO
-        LIBRARIES ws2_32 ${M_LIBRARIES}
+        LIBRARIES ws2_32 ${M_LIBRARIES} $<$<VERSION_GREATER_EQUAL:${PY_VERSION},3.8>:iphlpapi>
     )
 else()
     list(APPEND _socket_SOURCES timemodule.c)
@@ -290,6 +302,26 @@ set(ctypes_COMMON_SOURCES
     _ctypes/stgdict.c
     _ctypes/cfield.c
 )
+
+if(
+    (WIN32 AND PY_VERSION VERSION_GREATER_EQUAL "3.8")
+    OR
+    (LINUX AND PY_VERSION VERSION_GREATER_EQUAL "3.7")
+  )
+    # If installed, build ctypes using the system ffi
+    # * on Windows for python >= 3.8
+    # * on Linux for python >= 3.7
+    if(NOT USE_SYSTEM_FFI)
+        message(AUTHOR_WARNING "Disabling USE_SYSTEM_FFI option is *NOT* supported with Python >= 3.8 on windows and with Python >= 3.7 on Linux. Current version is ${PY_VERSION}")
+    endif()
+    add_python_extension(_ctypes
+        REQUIRES FFI_INCLUDE_DIR FFI_LIBRARY
+        SOURCES ${ctypes_COMMON_SOURCES}
+        DEFINITIONS Py_BUILD_CORE_MODULE FFI_BUILDING
+        INCLUDEDIRS ${FFI_INCLUDE_DIR}
+        LIBRARIES ${FFI_LIBRARY}
+    )
+else()
 # ctypes is different on Windows
 if(WIN32)
     if(MINGW)
@@ -337,7 +369,7 @@ else()
                         ${SRC_DIR}/Modules/_ctypes/darwin
             DEFINITIONS MACOSX
         )
-    elseif(PY_VERSION VERSION_LESS "3.7")
+    else()
         if("${CMAKE_SYSTEM_PROCESSOR}" STREQUAL "x86_64")
           set(_libffi_system_extra_src
                 _ctypes/libffi/src/${_libffi_system_dir}/ffi64.c
@@ -370,17 +402,8 @@ else()
                         ${INCLUDE_BUILD_DIR}      # For fficonfig.h
                         ${PROJECT_SOURCE_DIR}/cmake # For ffi.h
         )
-
-    else()
-        find_path(FFI_INCLUDE_DIR ffi.h)
-        find_library(FFI_LIBRARY NAMES ffi libffi)
-        add_python_extension(_ctypes
-            REQUIRES FFI_INCLUDE_DIR FFI_LIBRARY
-            SOURCES ${ctypes_COMMON_SOURCES}
-            INCLUDEDIRS ${FFI_INCLUDE_DIR}
-            LIBRARIES ${FFI_LIBRARY}
-        )
     endif()
+endif()
 endif()
 
 # Python3: _decimal

CMakeLists.txt
@@ -1,6 +1,6 @@
 cmake_minimum_required(VERSION 3.13.5)
 
-set(PYTHON_VERSION "3.7.12" CACHE STRING "The version of Python to build.")
+set(PYTHON_VERSION "3.8.5" CACHE STRING "The version of Python to build.")
 
 string(REPLACE "." ";" VERSION_LIST ${PYTHON_VERSION})
 list(GET VERSION_LIST 0 PY_VERSION_MAJOR)
@@ -227,7 +227,14 @@ set(_download_3.7.9_md5 "bcd9f22cf531efc6f06ca6b9b2919bd4")
 set(_download_3.7.10_md5 "0b19e34a6dabc4bf15fdcdf9e77e9856")
 set(_download_3.7.11_md5 "a7e66953dba909d395755b3f2e491f6e")
 set(_download_3.7.12_md5 "6fe83678c085a7735a943cf1e4d41c14")
-
+# 3.8.x
+set(_download_3.8.0_md5 "e18a9d1a0a6d858b9787e03fc6fdaa20")
+set(_download_3.8.1_md5 "f215fa2f55a78de739c1787ec56b2bcd")
+set(_download_3.8.2_md5 "f9f3768f757e34b342dbc06b41cbc844")
+set(_download_3.8.3_md5 "a7c10a2ac9d62de75a0ca5204e2e7d07")
+set(_download_3.8.4_md5 "387e63fe42c40a29e3408ce231315516")
+set(_download_3.8.5_md5 "e2f52bcf531c8cc94732c0b6ff933ff0")
+set(_download_3.8.6_md5 "ea132d6f449766623eee886966c7d41f")
 
 set(_extracted_dir "Python-${PY_VERSION}")
 
@@ -375,6 +382,22 @@ endif()
 message(STATUS "Setting PY_SSL_DEFAULT_CIPHER_STRING to '${WITH_SSL_DEFAULT_SUITES}'")
 endif()
 
+if(PY_VERSION VERSION_GREATER_EQUAL "3.8")
+    option(WITH_DECIMAL_CONTEXTVAR "Build _decimal module using a coroutine-local rather than a thread-local context" ON)
+else()
+    if(DEFINED WITH_C_LOCALE_COERCION)
+        message(AUTHOR_WARNING "WITH_DECIMAL_CONTEXTVAR option is *NOT* supported with Python < 3.8. Current version is ${PY_VERSION}")
+    endif()
+endif()
+
+if(PY_VERSION VERSION_GREATER_EQUAL "3.8")
+    option(WITH_TRACE_REFS "Enable tracing references for debugging purpose" OFF)
+else()
+    if(DEFINED WITH_TRACE_REFS)
+        message(AUTHOR_WARNING "WITH_TRACE_REFS option is *NOT* supported with Python < 3.8. Current version is ${PY_VERSION}")
+    endif()
+endif()
+
 # This is the major version number of Python
 set(LIBPYTHON_VERSION ${PY_VERSION_MAJOR}.${PY_VERSION_MINOR})
 if(WIN32)
@@ -466,6 +489,7 @@ endif()
 
 # Set include directories
 include_directories(${INCLUDE_BUILD_DIR})
+include_directories(${INCLUDE_BUILD_DIR}/internal)
 include_directories(${PYCONFIG_BUILD_DIR})
 include_directories(${SRC_DIR}/Python)
 
@@ -542,9 +566,15 @@ add_custom_target(clinic
 endif()
 
 # Add target to generate 'Include/graminit.h' and 'Python/graminit.c'
+if(PY_VERSION VERSION_GREATER_EQUAL "3.8")
+    set(generate_graminit_command $<TARGET_FILE:python> -m Parser.pgen)
+else()
+    set(generate_graminit_command $<TARGET_FILE:pgen>)
+endif()
 add_custom_target(generate_graminit
-    COMMAND ${CMAKE_CROSSCOMPILING_EMULATOR} $<TARGET_FILE:pgen>
+    COMMAND ${CMAKE_CROSSCOMPILING_EMULATOR} ${generate_graminit_command}
         ${SRC_DIR}/Grammar/Grammar
+        $<$<VERSION_GREATER_EQUAL:${PY_VERSION},3.8>:${SRC_DIR}/Grammar/Tokens>
         ${PROJECT_BINARY_DIR}/CMakeFiles/graminit.h
         ${PROJECT_BINARY_DIR}/CMakeFiles/graminit.c
     COMMAND ${CMAKE_COMMAND} -E copy_if_different
@@ -553,7 +583,7 @@ add_custom_target(generate_graminit
     COMMAND ${CMAKE_COMMAND} -E copy_if_different
         ${PROJECT_BINARY_DIR}/CMakeFiles/graminit.c
         ${SRC_DIR}/Python/graminit.c
-    DEPENDS pgen
+    DEPENDS $<IF:$<VERSION_GREATER_EQUAL:${PY_VERSION},3.8>,python,pgen>
     WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
     COMMENT "Generating '${SRC_DIR}/Include/graminit.h' and '${SRC_DIR}/Python/graminit.c'."
     VERBATIM

commit 324c46e8d662d867df77c22f8fe96da8681de4a9

3.8.x: Fix test_fcntl on macOS ensuring _posixshmem module is built
This commit addresses a regression introduced in 5cf5b7679 (Add support
for building Python 3.8.x) adding support for building extension _posixshmem
based of python/cpython@e5ef45b8f. It turns out module should be built on
both Unix (including macOS) and not only Linux.

It fixes error like the following reported when running 3.8.x or
3.9.x tests:

  ERROR: test_lockf_exclusive (test.test_fcntl.TestFcntl)

  Traceback (most recent call last):
    File "/Users/runner/work/python-cmake-buildsystem/python-cmake-buildsystem/build/lib/python3.8/test/test_fcntl.py", line 162, in test_lockf_exclusive
      p.start()
    File "/Users/runner/work/python-cmake-buildsystem/python-cmake-buildsystem/build/lib/python3.8/multiprocessing/process.py", line 121, in start
      self._popen = self._Popen(self)
    File "/Users/runner/work/python-cmake-buildsystem/python-cmake-buildsystem/build/lib/python3.8/multiprocessing/context.py", line 224, in _Popen
      return _default_context.get_context().Process._Popen(process_obj)
    File "/Users/runner/work/python-cmake-buildsystem/python-cmake-buildsystem/build/lib/python3.8/multiprocessing/context.py", line 284, in _Popen
      return Popen(process_obj)
    File "/Users/runner/work/python-cmake-buildsystem/python-cmake-buildsystem/build/lib/python3.8/multiprocessing/popen_spawn_posix.py", line 32, in __init__
      super().__init__(process_obj)
    File "/Users/runner/work/python-cmake-buildsystem/python-cmake-buildsystem/build/lib/python3.8/multiprocessing/popen_fork.py", line 19, in __init__
      self._launch(process_obj)
    File "/Users/runner/work/python-cmake-buildsystem/python-cmake-buildsystem/build/lib/python3.8/multiprocessing/popen_spawn_posix.py", line 39, in _launch
      from . import resource_tracker

cmake/ConfigureChecks.cmake
@@ -1041,37 +1041,13 @@ endif()
 #######################################################################
 if(PY_VERSION VERSION_GREATER_EQUAL "3.8")
 
-function(_write_shm_test func test_filepath_var)
-  set(check_src ${PROJECT_BINARY_DIR}/CMakeFiles/${func}.c)
-  file(WRITE ${check_src} "
-    #ifdef __cplusplus
-    extern \"C\"
-    #endif
-    #ifdef HAVE_SYS_MMAN_H
-    #  include <sys/mman.h>
-    #endif
-    int main() { return ${func}(0, NULL); }
-  ")
-  set(${test_filepath_var} ${check_src} PARENT_SCOPE)
-endfunction()
 foreach(func IN ITEMS shm_open shm_unlink)
-  _write_shm_test(${func} check_src)
   string(TOUPPER ${func} _func_upper)
-  python_platform_test(
-    HAVE_${_func_upper}
-    "Checking for ${func}"
-    ${check_src}
-    DIRECT
-    )
+  check_symbol_exists(${func} "${CFG_HEADERS}" HAVE_${_func_upper})
   if(NOT HAVE_${_func_upper})
     cmake_push_check_state()
     list(APPEND CMAKE_REQUIRED_LIBRARIES rt)
-    python_platform_test(
-      HAVE_${_func_upper}_IN_RT
-      "Checking for ${func} in -lrt"
-      ${check_src}
-      DIRECT
-      )
+    check_symbol_exists(${func} "${CFG_HEADERS}" HAVE_${_func_upper}_IN_RT)
     cmake_pop_check_state()
     set(SHM_NEEDS_LIBRT ${HAVE_${_func_upper}_IN_RT})
     set(HAVE_${_func_upper} ${HAVE_${_func_upper}_IN_RT})

cmake/extensions/CMakeLists.txt
@@ -197,9 +197,13 @@ add_python_extension(_scproxy
 add_python_extension(linuxaudiodev REQUIRES LINUX IS_PY2 SOURCES linuxaudiodev.c)
 add_python_extension(ossaudiodev REQUIRES LINUX SOURCES ossaudiodev.c)
 
-# Python 3.8: Linux-only extensions
+# Python 3.8: UNIX-only extensions
 if(PY_VERSION VERSION_GREATER_EQUAL "3.8")
-    add_python_extension(_posixshmem REQUIRES LINUX SOURCES _multiprocessing/posixshmem.c LIBRARIES "rt")
+    set(_posixshmem_LIBRARIES)
+    if(HAVE_SHM_OPEN_IN_RT)
+        list(APPEND _posixshmem_LIBRARIES rt)
+    endif()
+    add_python_extension(_posixshmem REQUIRES UNIX SOURCES _multiprocessing/posixshmem.c LIBRARIES ${_posixshmem_LIBRARIES})
 endif()
 
 # Windows-only extensions